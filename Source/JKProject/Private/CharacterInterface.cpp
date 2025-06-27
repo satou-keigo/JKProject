a3705d5dbe5d3e142fdc81a03612b78f2fb4f2ff
satou-keigo <24CI0412@jec.ac.jp>
2025-06-27 12:30:48 +0900

    C++の作成

---
+++ Source/JKProject/Private/CharacterInterface.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CharacterInterface.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+//‰¼ŠÖ”
+void shot()
+{
+	UKismetSystemLibrary::PrintString(GEngine->GetWorld(), "shot", true, true, FLinearColor(0.0f, 0.66f, 1.0f, 1.0f), 2.0f);
+	UKismetSystemLibrary::PrintString(GEngine->GetWorld(), "shot");
+}
+
+// Add default functionality here for any ICharacterInterface functions that are not pure virtual.
