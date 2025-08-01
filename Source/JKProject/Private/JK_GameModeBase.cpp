e540d3a1278fc5f2937bf92831eb3ceae24af460
satou-keigo <24CI0412@jec.ac.jp>
2025-08-01 11:23:43 +0900

    ゲームモードベース受け取れ

---
+++ Source/JKProject/Private/JK_GameModeBase.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "JK_GameModeBase.h"
+
+AJK_GameModeBase::AJK_GameModeBase()
+{
+	DefaultPawnClass = APlayerCharacter::StaticClass();
+}
---
+++ Source/JKProject/Public/JK_GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PlayerCharacter.h"
+#include "JK_GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JKPROJECT_API AJK_GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	AJK_GameModeBase();
+};
